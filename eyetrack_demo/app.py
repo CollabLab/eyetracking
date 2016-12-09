d448905354e6f211e67720f70b39706cb9c11876
Scott Allen Cambo <scottcambo2019@u.northwestern.edu>
2016-12-09 12:37:11 -0600

    modified print statements for python 3 compatibility

--- eyetrack_demo/app.py
+++ eyetrack_demo/app.py
@@ -12,8 +12,8 @@ import pickle
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
-async_mode = "gevent"
-
+#async_mode = "gevent"
+async_mode = None
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
